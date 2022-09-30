e10fb95698960a618b4a1e0b527110981a34e8f2
Dawid Zawada <dawidzawada@MacBook-Pro-Dawid.local>
2022-09-30 12:23:07 +0200

    Initial commit

---
+++ ios/ArticleTest4Clip/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ArticleTest4Clip
+//
+//  Created by Dawid Zawada on 30/09/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
